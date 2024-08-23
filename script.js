9fd0f3c78b422976fc670d2e2f0af27c5e567de7
rakesh <rakeshkdmb41@gmail.com>
2024-08-23 21:04:04 +0530

    add playRound() function to play the game one round create humanScore and computerScore variables to store scores create score array to return two score at a time from playRound() function

--- script.js
+++ script.js
@@ -22,3 +22,43 @@ function getHumanChoice() {
     return 'rock'
   }
 }
+
+let humanScore = 0
+let computerScore = 0
+let score = [0, 0]
+
+function playRound(humanChoice, computerChoice) {
+  if (computerChoice === 'rock') {
+    if (humanChoice === 'rock') {
+      console.log("It's Draw! Rock Rock")
+    } else if (humanChoice === 'paper') {
+      console.log('You win! Paper beats Rock')
+      humanScore++
+    } else if (humanChoice === 'scissors') {
+      console.log('You lose! Rock beats Scissors')
+      computerScore++
+    }
+  } else if (computerChoice === 'paper') {
+    if (humanChoice === 'rock') {
+      console.log('You lose! Paper beats Rock')
+      computerScore++
+    } else if (humanChoice === 'paper') {
+      console.log("It's Draw! Paper Paper")
+    } else if (humanChoice === 'scissors') {
+      console.log('You win! Scissors beats Paper')
+      humanScore++
+    }
+  } else if (computerChoice === 'scissors') {
+    if (humanChoice === 'rock') {
+      console.log('You win! Rock beats Scissors')
+      humanScore++
+    } else if (humanChoice === 'paper') {
+      console.log('You lose! Scissors beats Paper')
+      computerScore++
+    } else if (humanChoice === 'scissors') {
+      console.log("It's Draw! Scissors Scissors")
+    }
+  }
+  score = [computerScore, humanScore]
+  return score
+}
