b681dd629e7438101b67b25f8333f0fbf58e5c9c
rakesh <rakeshkdmb41@gmail.com>
2024-08-24 16:24:37 +0530

    set each round result in roundWinner as a textContent which was previously showing in console

--- script.js
+++ script.js
@@ -17,33 +17,33 @@ let score = [0, 0]
 function playRound(humanChoice, computerChoice) {
   if (computerChoice === 'rock') {
     if (humanChoice === 'rock') {
-      console.log("It's Draw! Rock Rock")
+      roundWinner.textContent = "It's Draw! Rock Rock"
     } else if (humanChoice === 'paper') {
-      console.log('You win! Paper beats Rock')
+      roundWinner.textContent = 'You win! Paper beats Rock'
       humanScore++
     } else if (humanChoice === 'scissors') {
-      console.log('You lose! Rock beats Scissors')
+      roundWinner.textContent = 'You lose! Rock beats Scissors'
       computerScore++
     }
   } else if (computerChoice === 'paper') {
     if (humanChoice === 'rock') {
-      console.log('You lose! Paper beats Rock')
+      roundWinner.textContent = 'You lose! Paper beats Rock'
       computerScore++
     } else if (humanChoice === 'paper') {
-      console.log("It's Draw! Paper Paper")
+      roundWinner.textContent = "It's Draw! Paper Paper"
     } else if (humanChoice === 'scissors') {
-      console.log('You win! Scissors beats Paper')
+      roundWinner.textContent = 'You win! Scissors beats Paper'
       humanScore++
     }
   } else if (computerChoice === 'scissors') {
     if (humanChoice === 'rock') {
-      console.log('You win! Rock beats Scissors')
+      roundWinner.textContent = 'You win! Rock beats Scissors'
       humanScore++
     } else if (humanChoice === 'paper') {
-      console.log('You lose! Scissors beats Paper')
+      roundWinner.textContent = 'You lose! Scissors beats Paper'
       computerScore++
     } else if (humanChoice === 'scissors') {
-      console.log("It's Draw! Scissors Scissors")
+      roundWinner.textContent = "It's Draw! Scissors Scissors"
     }
   }
   score = [humanScore, computerScore]
